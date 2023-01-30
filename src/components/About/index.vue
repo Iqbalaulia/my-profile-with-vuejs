0ec92cf1fe091cdb904734445b667f6afa631754
iqbalaulia <iqbal.developer16@gmail.com>
2023-01-30 21:03:32 +0700

    about section

---
+++ src/components/About/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="about">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="about_text">
+          About Me
+          <div class="line"></div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="about_description">
+            <label for="">Hai, Saya Muhammad Iqbal Aulia Rafi,</label>
+            <p>
+              Seorang <strong>Front-end Web Developer</strong> yang kreatif
+              dengan memiliki pengalaman profesional lebih dari 2 tahun.
+              Memiliki kemampuan dan pengalaman dalam merancang, mengembangkan
+              website dengan teknologi terkini. Dapat menggunakan bahasa
+              pemrograman Javascript, HTML , SCSS, Vue JS dan Nuxt.JS. Senang
+              mempelajari hal - hal baru yang berkaitan dengan spesialis sebagai
+              Front-end Developer
+            </p>
+            <p>
+              Memulai karir pada tahun 2017 di sebuah kampus terkenal yang ada
+              di kota Surabaya, yaitu STIKOM Surabaya yang sekarang berganti
+              nama menjadi Universitas Dinamika. Kecintaan saya menjadi seorang
+              <strong>Front-end Web Developer</strong> bermula pada mata
+              pelajaran Pemrograman Web. Pada saat awal , diajarkan oleh dosen
+              saya tentang penggunaan Javascript. Setelah itu diajarkan
+              mendesain sebuah tampilan sederhana. Saat diberi tugas untuk
+              mendesain sebuah tampilan website sederhana. Dari situlah muncul
+              rasa senang, bahagia dan begitu serius mengerjakannya sehingga
+              pada saat selesai muncul rasa <strong>"sesuatu"</strong>. Mulai
+              saat itulah perlahan - lahan saya belajar lebih dalam lagi tentang
+              menjadi <strong>Front-end Web Developer</strong> hingga saat ini.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AboutMe",
+};
+</script>
--- src/views/Homepage/index.vue
+++ src/views/Homepage/index.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    
+    <SectionAbout />
   </Layout>
 </template>
 <script>
@@ -8,6 +8,7 @@ export default {
   name: "homepage",
   components: {
     Layout: () => import("@/views/Layout"),
+    SectionAbout: () => import("@/components/About"),
   },
 };
 </script>
