d7aaa67987c110a772170b010de7e7a9d91de128
iqbalaulia <iqbal.developer16@gmail.com>
2023-01-30 08:52:33 +0700

    initial project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./utils/method";
 
-Vue.config.productionTip = false
+/* Initial master style */
+import { PaginationPlugin, DropdownPlugin, NavPlugin } from "bootstrap-vue";
+Vue.use(DropdownPlugin);
+Vue.use(PaginationPlugin);
+Vue.use(NavPlugin);
+
+/** Initial master component*/
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/en";
+Vue.use(ElementUI, { locale });
+
+/* Global base css */
+import "../src/assets/scss/base/index.scss";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
