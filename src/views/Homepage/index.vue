ba95ecaf39832f89e505f12f90c3e45f3d0e1728
iqbalaulia <iqbal.developer16@gmail.com>
2023-04-30 23:53:58 +0700

    [repair ui] layout base

--- src/views/Homepage/index.vue
+++ src/views/Homepage/index.vue
@@ -1,20 +1,22 @@
+<!-- @format -->
+
 <template>
-  <Layout>
-    <SectionAbout />
-    <SectionJourney />
-    <SectionWorking />
-    <SectionMe />
-  </Layout>
+	<Layout>
+		<!-- <SectionAbout />
+		<SectionJourney />
+		<SectionWorking />
+		<SectionMe /> -->
+	</Layout>
 </template>
 <script>
 export default {
-  name: "homepage",
-  components: {
-    Layout: () => import("@/views/Layout"),
-    SectionAbout: () => import("@/components/About"),
-    SectionJourney: () => import("@/components/Journey"),
-    SectionMe: () => import("@/components/Me"),
-    SectionWorking: () => import("@/components/Working"),
-  },
+	name: "homepage",
+	components: {
+		Layout: () => import("@/views/Layout"),
+		// SectionAbout: () => import("@/components/About"),
+		// SectionJourney: () => import("@/components/Journey"),
+		// SectionMe: () => import("@/components/Me"),
+		// SectionWorking: () => import("@/components/Working"),
+	},
 };
 </script>
--- src/views/Layout/Navbar/index.vue
+++ src/views/Layout/Navbar/index.vue
@@ -1,22 +1,30 @@
+<!-- @format -->
+
 <template>
-  <div>
-    <b-navbar toggleable="lg" class="nav-my-profile">
-      <div class="container">
-        <!-- <b-navbar-brand href="#">NavBar</b-navbar-brand> -->
+	<div>
+		<b-navbar toggleable="lg" class="nav-my-profile">
+			<div class="container">
+				<!-- <b-navbar-brand href="#">NavBar</b-navbar-brand> -->
+				<b-navbar-brand class="logo-custom" href="#">
+					<img
+						src="@/assets/images/logo/logo_iqbal_transparent.png"
+						class="d-inline-block align-top"
+						alt="Logo"
+						v-once />
+				</b-navbar-brand>
+				<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="text-center">
-            <b-nav-item href="#">About</b-nav-item>
-            <b-nav-item href="#">Journey</b-nav-item>
-            <b-nav-item href="#">Work</b-nav-item>
-            <b-nav-item href="#">Photography</b-nav-item>
-            <b-nav-item href="#">Journal</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </div>
-    </b-navbar>
-  </div>
+				<b-collapse id="nav-collapse" is-nav>
+					<!-- Right aligned nav items -->
+					<b-navbar-nav class="text-center">
+						<b-nav-item class="nav-item-active" href="#">Home</b-nav-item>
+						<b-nav-item href="#">Journey</b-nav-item>
+						<b-nav-item href="#">Work</b-nav-item>
+						<b-nav-item href="#">Photography</b-nav-item>
+						<b-nav-item href="#">Journal</b-nav-item>
+					</b-navbar-nav>
+				</b-collapse>
+			</div>
+		</b-navbar>
+	</div>
 </template>
