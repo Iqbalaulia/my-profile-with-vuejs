f9c9518736c3ada17805905ea6d9b15697f5f3a9
iqbalaulia <iqbal.developer16@gmail.com>
2023-05-23 01:21:22 +0700

    update: vue meta

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
+/** @format */
+
 import Vue from "vue";
 import VueRouter from "vue-router";
+import VueMeta from "vue-meta";
+
 // import store from "../store";
 Vue.use(VueRouter);
+Vue.use(VueMeta);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: () => import(/* webpackChunkName: "home" */ "../views/Homepage"),
-    meta: {
-      title: "Homepage | ",
-    },
-  },
+	{
+		path: "/",
+		name: "Home",
+		component: () => import(/* webpackChunkName: "home" */ "../views/Homepage"),
+		metaInfo: {
+			title: "Default App Title",
+			titleTemplate: "%s | vue-meta Example App",
+			htmlAttrs: {
+				lang: "en-US",
+			},
+			meta: [
+				{ charset: "utf-8" },
+				{
+					name: "description",
+					content: "An example Vue application with vue-meta.",
+				},
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			],
+		},
+	},
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  routes,
+	mode: "history",
+	routes,
 });
-
 
 export default router;
