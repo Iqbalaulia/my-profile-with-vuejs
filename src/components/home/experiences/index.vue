17ff5e087c2cceb63f0a5780bea33a6937028aa3
iqbalaulia <miqbalar12@gmail.com>
2023-09-23 22:46:51 +0700

    fixing: data from json experience

--- src/components/home/experiences/index.vue
+++ src/components/home/experiences/index.vue
@@ -11,120 +11,17 @@
 				<h4>Frontend Engineer</h4>
 			</div>
 
-			<div class="timeline">
+			<div class="timeline" v-for="(experience, indexExperience) in data" :key="indexExperience">
 				<div class="timeline-item">
 					<div class="timeline-icon"> </div>
-					<div class="timeline-content">
-						<p class="timeline-content-date">Sep 2021 - Now</p>
-						<label for="" class="timeline-content-position"
-							>Frontend Web Developer</label
-						>
-						<div class="timeline-content-company"
-							>PT Sinergi Informatika Semen Indonesia (SISI)</div
-						>
+					<div class="timeline-content" :class="experience.section">
+						<p class="timeline-content-date">{{experience.start_date}} - {{experience.end_date ? experience.end_date : `Now`}}</p>
+							<label for="" class="timeline-content-position">
+								{{experience.position}}
+							</label>
+							<div class="timeline-content-company">{{experience.company}}</div>
 						<p>
-							The company offers a wide range of IT services, including
-							infrastructure management, application development and
-							maintenance, system integration, database administration, IT
-							security, and business intelligence. These services help Semen
-							Indonesia to improve its efficiency, productivity, and customer
-							satisfaction.
-						</p>
-					</div>
-				</div>
-
-				<div class="timeline-item">
-					<div class="timeline-icon"> </div>
-					<div class="timeline-content right">
-						<p class="timeline-content-date">Sep 2022 - Mei 2023</p>
-						<label for="" class="timeline-content-position"
-							>Frontend Web Developer
-						</label>
-						<div class="timeline-content-company">GoCement </div>
-						<p>
-							The leading B2B marketplace for building materials in Indonesia,
-							providing building materials for various construction needs and
-							digitizing the building materials store. Currently, many building
-							stores in East Java have joined as GoCement partners.</p
-						>
-					</div>
-				</div>
-
-				<div class="timeline-item">
-					<div class="timeline-icon"> </div>
-					<div class="timeline-content">
-						<p class="timeline-content-date">Sep 2022 - Mei 2023</p>
-						<label for="" class="timeline-content-position"
-							>Frontend Web Developer
-						</label>
-						<div class="timeline-content-company"
-							>PT Onklas Prima Indonesia
-						</div>
-						<p
-							>Onklas is the #1 Learning System, Social Media, and School
-							Examination platform in Indonesia. Onklas supports educational
-							growth and creates ease in learning, and has been trusted by
-							various schools throughout Indonesia. Teachers can remain
-							productive and creative while teaching through the online learning
-							system, where assignments, materials, and exams can be done
-							through the Onklas application and website.</p
-						>
-					</div>
-				</div>
-
-				<div class="timeline-item">
-					<div class="timeline-icon"> </div>
-					<div class="timeline-content right">
-						<p class="timeline-content-date">Sep 2020 - Jan 2021</p>
-						<label for="" class="timeline-content-position"
-							>Frontend Web Developer
-						</label>
-						<div class="timeline-content-company">GoCement </div>
-						<p>
-							KLIK Chat Bisnis is a Chatting application that connects customers
-							to customer service. Not only that, KLIK Chat Bisnis can also
-							create an application that answers user questions without having
-							to be answered by customer service. However, there is a problem in
-							that this application is only available on the web platform. This
-							makes customer service unable to be mobile in answering chats
-							because they have to stay in front of a personal computer.</p
-						>
-					</div>
-				</div>
-
-				<div class="timeline-item">
-					<div class="timeline-icon"> </div>
-					<div class="timeline-content">
-						<p class="timeline-content-date">Jul 2019 - Feb 2020</p>
-						<label for="" class="timeline-content-position"
-							>Fullstack Web Developer
-						</label>
-						<div class="timeline-content-company">Peduly </div>
-						<p
-							>Peduly is a startup that operates in the field of
-							humanitarianism. Peduly acts as a third party in the fundraising
-							process, connecting campaign creators or those in need of funds
-							with funders. Based on crowdfunding, it is located in Surabaya,
-							East Java.</p
-						>
-					</div>
-				</div>
-
-				<div class="timeline-item">
-					<div class="timeline-icon"> </div>
-					<div class="timeline-content right">
-						<p class="timeline-content-date">Agu 2019 - Nov 2019</p>
-						<label for="" class="timeline-content-position"
-							>Frontend Web Developer
-						</label>
-						<div class="timeline-content-company"
-							>SmartAircraft AeroSmartAircraft Aero
-						</div>
-						<p>
-							SmartAircraft Aero is a company operating in the aviation
-							industry, located in the area of Juanda Airport. The company
-							provides a special internal web for all aviation needs of an
-							airline.
+							{{experience.information_company}}
 						</p>
 					</div>
 				</div>
@@ -134,5 +31,13 @@
 </template>
 
 <script>
-export default {};
+import jsonExperience from './data/experience.json'
+
+export default {
+	data() {
+		return {
+			data: jsonExperience.data.data
+		}
+	}
+};
 </script>
